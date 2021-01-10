e9d611e79db651abaf683c15a62629f86b4e2af3
Ryan <webdev2080@gmail.com>
2021-01-10 11:47:06 -0800

    Employee Database Access Object, interface used to allow DB access.

--- RSM-App/src/main/java/com/ryanhelferich/springboot/springboot/dao/EmployeeDAO.java
+++ RSM-App/src/main/java/com/ryanhelferich/springboot/springboot/dao/EmployeeDAO.java
@@ -1,5 +1,12 @@
 package com.ryanhelferich.springboot.springboot.dao;
 
-public class EmployeeDAO {
+import java.util.List;
 
+import com.ryanhelferich.springboot.springboot.modal.Employee;
+
+public interface EmployeeDAO {
+	List<Employee> get();
+	Employee get(int id);
+	void save(Employee employee);
+	void delete(int id);
 }
