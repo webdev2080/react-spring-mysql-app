a191720aa388bba0d35284e5f45b2d540f6e46b8
Ryan <webdev2080@gmail.com>
2021-01-10 12:36:40 -0800

    Employee Service Implementation Setup

--- RSM-App/src/main/java/com/ryanhelferich/springboot/springboot/service/EmployeeServiceImp.java
+++ RSM-App/src/main/java/com/ryanhelferich/springboot/springboot/service/EmployeeServiceImp.java
@@ -1,5 +1,41 @@
 package com.ryanhelferich.springboot.springboot.service;
 
-public class EmployeeServiceImp {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ryanhelferich.springboot.springboot.dao.EmployeeDAO;
+import com.ryanhelferich.springboot.springboot.modal.Employee;
+
+@Service
+public class EmployeeServiceImp implements EmployeeService {
+
+	@Autowired
+	private EmployeeDAO employeeDao;
+	
+	@Transactional
+	@Override
+	public List<Employee> get() {
+		return employeeDao.get();
+	}
+	
+	@Transactional
+	@Override
+	public Employee get(int id) {
+		return employeeDao.get(id);
+	}
+	
+	@Transactional
+	@Override
+	public void save(Employee employee) {
+		employeeDao.save(employee);
+	}
+	
+	@Transactional
+	@Override
+	public void delete(int id) {
+		employeeDao.delete(id);
+	}
 }
