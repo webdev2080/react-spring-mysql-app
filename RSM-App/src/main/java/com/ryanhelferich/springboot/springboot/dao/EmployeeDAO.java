de4d0d1239efbd2593a1406259930538f13de908
Ryan <webdev2080@gmail.com>
2021-01-10 12:12:20 -0800

    Implementation of the Employee DAO interface.

--- RSM-App/src/main/java/com/ryanhelferich/springboot/springboot/dao/EmployeeDAO.java
+++ RSM-App/src/main/java/com/ryanhelferich/springboot/springboot/dao/EmployeeDAO.java
@@ -6,7 +6,10 @@ import com.ryanhelferich.springboot.springboot.modal.Employee;
 
 public interface EmployeeDAO {
 	List<Employee> get();
+	
 	Employee get(int id);
+	
 	void save(Employee employee);
+	
 	void delete(int id);
 }
